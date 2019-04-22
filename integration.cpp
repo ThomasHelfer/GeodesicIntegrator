80932ea7d89e0f45c7c493aed6078459b50d47bb
Thomas Helfer <dc-helf1@login-e-16.data.cluster>
2019-04-22 13:46:40 +0100

    Separated out rk4 into separte file

--- integration.cpp
+++ integration.cpp
@@ -12,6 +12,7 @@
 
 #include "tensor.hpp"
 #include "schwarzschild.hpp"
+#include "rk4.hpp"
 
 using namespace std;
 
@@ -23,20 +24,6 @@ using namespace std;
 
 #define H 50
 
-auto rk4(Vec3 f(double, Vec3))
-{
-        return
-        [       f            ](double t, Vec3 y, double dt ) -> Vec3{ return
-        [t,y,dt,f            ](                    Vec3 dy1) -> Vec3{ return
-        [t,y,dt,f,dy1        ](                    Vec3 dy2) -> Vec3{ return
-        [t,y,dt,f,dy1,dy2    ](                    Vec3 dy3) -> Vec3{ return
-        [t,y,dt,f,dy1,dy2,dy3](                    Vec3 dy4) -> Vec3{ return
-        ( dy1 + dy2*2 + dy3*2 + dy4 ) / 6   ;} (
-        f( t+dt  , y+dy3   )*dt          );} (
-        f( t+dt/2, y+dy2/2 )*dt          );} (
-        f( t+dt/2, y+dy1/2 )*dt          );} (
-        f( t     , y       )*dt          );} ;
-}
 
 template <typename data_t>
 class render_black_hole{
