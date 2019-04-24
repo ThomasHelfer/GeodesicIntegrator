55a74ca632497f21c1a21c5485683bca94d9e9cf
Thomas <thomashelfer@live.de>
2019-04-24 07:29:14 +0100

    separated geodesic_shooting class into different file

---
+++ geodesic_shooter.hpp
@@ -0,0 +1,41 @@
+
+
+template <typename data_t>
+class geodesic_shooter{
+        public:
+        void shoot(Vec3 center, double shift = 1/5., int shoot = 10 , 
+		   const double TIME_MAXIMUM = 150.0, const double T_START = 0.0, const double DT = 0.1){
+
+                data_t metric;
+                auto dy = rk4( metric.eval_diff_eqn ) ;
+
+                for(int i = 0; i<shoot ; i++){
+
+                        const Vec3 Y_START(0.0, 7+i/5., 0.0, 0.0, -1.0, 0.00, 0.0, -1.0);
+                        Vec3 y = Y_START + center;
+                        double t = T_START;
+
+                        // ========= Preparing output ==========
+                        string imgname="xpos";
+                        char cbuff[20];
+                        sprintf (cbuff, "%03d", i);
+                        imgname.append(cbuff);
+                        imgname.append(".csv");
+                        ofstream myfile(imgname);
+
+                        // ========== Integration and output ===
+                        while(t <= TIME_MAXIMUM) {
+                                y = y + dy(t,y,DT) ; t += DT;
+
+                                myfile << y.x <<"       " << y.y << "   " << y.z << "   " << y.t << "   " << metric.calculate_norm(y) <<   endl;
+                        }
+
+                        // ========== clean up ==================
+                        myfile.close();
+
+                }
+
+        }
+
+
+};
--- integration.cpp
+++ integration.cpp
@@ -14,6 +14,7 @@
 #include "schwarzschild.hpp"
 #include "rk4.hpp"
 #include "render.hpp"
+#include "geodesic_shooter.hpp"
 
 using namespace std;
 
@@ -21,45 +22,6 @@ using namespace std;
 #define FOR2(IDX1,IDX2) FOR1(IDX1) FOR1(IDX2)
 #define FOR3(IDX1,IDX2,IDX3) FOR2(IDX1,IDX2) FOR1(IDX3)
 #define FOR4(IDX1,IDX2,IDX3,IDX4) FOR2(IDX1,IDX2) FOR2(IDX3,IDX4)
-
-template <typename data_t> 
-class geodesic_shooter{
-	public:
-	void shoot(Vec3 center, double shift = 1/5., int shoot = 10 ,  const double TIME_MAXIMUM = 150.0, const double T_START = 0.0, const double DT = 0.1){
-
-		data_t metric;
-		auto dy = rk4( metric.eval_diff_eqn ) ;
-
-        	for(int i = 0; i<shoot ; i++){			
-
-			const Vec3 Y_START(0.0, 7+i/5., 0.0, 0.0, -1.0, 0.00, 0.0, -1.0);
-        		Vec3 y = Y_START + center;
-			double t = T_START;
-
-			// ========= Preparing output ==========
- 			string imgname="xpos";
-                	char cbuff[20];
-                	sprintf (cbuff, "%03d", i);
-                	imgname.append(cbuff);
-                	imgname.append(".csv");
-			ofstream myfile(imgname);
-
-			// ========== Integration and output ===
-        		while(t <= TIME_MAXIMUM) {
-  	  			y = y + dy(t,y,DT) ; t += DT;			
-
-				myfile << y.x <<"	" << y.y << "	" << y.z << "	" << y.t << "	" << metric.calculate_norm(y) <<   endl;
-			}
-
-			// ========== clean up ==================
-			myfile.close();
-
-		}
-	
-	}
-
-
-};
 
 int main(void)
 {
