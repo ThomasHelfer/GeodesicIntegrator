1bb8e630b3218b47482b4eeedddee797c08c03d9
Thomas Helfer <tom@tomtom.localdomain>
2020-09-07 19:03:03 +0200

    Cleaning repo and update .gitingore

--- MPI_run.sh
+++
@@ -1,5 +0,0 @@
-mpicc -std=c++14 integration_parallel.cpp
-mpirun -n 32 ./a.out
-for f in Image*.ppm; do
-  convert ./"$f" ./"${f%.ppm}.png"
-done
--- run.sh
+++
@@ -1,3 +0,0 @@
-g++ integration.cpp -O3
-./a.out
-python plot.py
